dcaae407ae27c8727c423f5e23bf390acc802dc3
ftakao2007 <ftakao2007@gmail.com>
2021-03-26 10:23:11 +0000

    update

--- build/symbolnode/digitalocean_centos8_symbolnode.sh
+++ build/symbolnode/digitalocean_centos8_symbolnode.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-ansible-playbook -i ~/hosts symbolnode.centos8_digitalocean.yml -u centos --private-key=~/.ssh/id_rsa
+
+### init
+ansible-playbook -i ~/hosts digitalocean_centos8_symbolnode.yml -u centos --private-key=~/.ssh/id_rsa
+
+### setup_testnet
+#ansible-playbook -i ~/hosts digitalocean_centos8_symbolnode_testnet.yml -u centos --private-key=~/.ssh/id_rsa
+
+### setup_mainnet
+#ansible-playbook -i ~/hosts symbolnode.centos8_digitalocean.yml -u centos --private-key=~/.ssh/id_rsa
+
+
