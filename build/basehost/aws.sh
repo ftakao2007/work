daf723039ff8a7d75ee01e71ccf0de4332942e66
Cloud User <ec2-user@ip-172-30-0-62.ap-northeast-1.compute.internal>
2021-03-26 22:06:16 +0000

    update

---
+++ build/basehost/aws.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+user=ec2-user
+#key=
+#address=
+
+echo "private key path?"
+ls ~/.ssh
+read key
+echo ""
+
+echo "------------------------"
+echo "address?"
+cat ~/hosts
+read address
+echo ""
+
+# test
+#ssh -i ~/.ssh/${key} ${user}@${address}
+#exit
+
+# common
+#ansible-playbook -i ~/hosts 00_only_ansible_aws.yml -u ${user} --private-key=~/.ssh/${key}
+ansible-playbook -i ~/hosts 01_common_centos8_aws.yml -u ${user} --private-key=~/.ssh/${key}
+
---
+++ build/symbolnode/aws_symbolnode.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+user=ec2-user
+#key=
+#address=
+
+echo "private key path?"
+ls ~/.ssh
+read key
+echo ""
+
+echo "------------------------"
+echo "address?"
+cat ~/hosts
+read address
+echo ""
+
+# test
+#ssh -i ~/.ssh/${key} ${user}@${address}
+#exit
+
+# init_base
+# ansible-playbook -i ~/hosts aws_centos8_symbolnode.yml -u ${user} --extra-vars "ex_var_passwd=${pass} --private-key=~/.ssh/${key}
+
+# init_bootstrap
+echo "------------------------"
+echo "passwd?"
+read pass
+echo ""
+ansible-playbook -i ~/hosts aws_centos8_symbolnode_bootstrap_testnet.yml -u ${user} --extra-vars "ex_var_passwd=${pass}" --private-key=~/.ssh/${key}
+#ansible-playbook -i ~/hosts aws_centos8_symbolnode_bootstrap_mainnet.yml -u ${user} --extra-vars "ex_var_passwd=${pass}" --private-key=~/.ssh/${key}
