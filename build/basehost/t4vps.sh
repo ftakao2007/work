012b6d0ede26c9fcfd0692299ab85835f999ce85
ftakao2007 <ftakao2007@gmail.com>
2021-04-01 11:22:19 +0000

    update

---
+++ build/basehost/t4vps.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+user=ftakao2007
+#key=
+#address=
+
+echo "private key?"
+ls ~/.ssh | egrep -v "known_hosts|authorized_keys"
+read key
+echo ""
+
+echo "------------------------"
+echo "address?"
+cat ~/hosts
+read address
+echo ""
+
+# test
+#ssh -i ~/.ssh/${key} ${user}@${address}
+#exit
+
+# common
+ansible-playbook -i ~/hosts 00_only_ansible_t4vps.yml -u ${user} --private-key=~/.ssh/${key} -K
+#ansible-playbook -i ~/hosts 01_common_centos8_t4vps.yml -u ${user} --private-key=~/.ssh/${key} -K
+
