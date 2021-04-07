70a929593eed8119544df55a6ddccbd5298201b7
ftakao2007 <ftakao2007@gmail.com>
2021-04-07 19:11:30 +0000

    update

--- build/symbolnode/digitalocean_centos8_symbolnode.sh
+++ build/symbolnode/digitalocean_centos8_symbolnode.sh
@@ -3,41 +3,33 @@
 user=centos
 #key=
 #address=
-
-echo "private key path?"
-ls ~/.ssh
-read key
-echo ""
-
-echo "------------------------"
-echo "address?"
-cat ~/hosts
-read address
-echo ""
+. ./setup_params_symbolnode
 
 # test
 #ssh -i ~/.ssh/${key} ${user}@${address}
 #exit
 
+
+#symbol_bootstrap_version="1.0.3"
+#symbol_cli_version="1.0.0"
+. ./setup_params_symbolnode_version
+
 # init_base
-ansible-playbook -i ~/hosts digitalocean_centos8_symbolnode.yml -u ${user} --private-key=~/.ssh/${key}
+ansible-playbook -i ~/hosts digitalocean_centos8_symbolnode.yml -u ${user} --private-key=~/.ssh/${key} \
+                 --extra-vars "ex_var_symbol_bootstrap_version=${symbol_bootstrap_version} \
+                               ex_var_symbol_cli_version=${symbol_cli_version} \
+                              "
+
 exit
 
-# init_bootstrap
-echo "------------------------"
-echo "bootstrap passwd?"
-read pass
-echo ""
-echo "------------------------"
-echo "beneficiaryAddress?"
-read bfaddress
-echo ""
-echo "------------------------"
-echo "symbol_network? [ mainnet / testnet ]"
-read symbol_network
-echo ""
+#pass=
+#bfaddress=
+#symbol_network=
+. ./setup_params_symbolnode_bootstrap
+exit
 
-ansible-playbook -i ~/hosts digitalocean_centos8_symbolnode_bootstrap_make_workdir.yml -u ${user} --private-key=~/.ssh/${key} \
-                 --extra-vars "ex_var_passwd=${pass} \
-                               ex_var_bfaddress=${bfaddress} \
-                               ex_var_symbol_network=${symbol_network} \
+#ansible-playbook -i ~/hosts digitalocean_centos8_symbolnode_bootstrap_make_workdir.yml -u ${user} --private-key=~/.ssh/${key} \
+#                 --extra-vars "ex_var_passwd=${pass} \
+#                               ex_var_bfaddress=${bfaddress} \
+#                               ex_var_symbol_network=${symbol_network} \
+#                              "
