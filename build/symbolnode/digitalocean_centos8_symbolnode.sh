19fe9e3ab34a1d50174dc20fa6554ea10516b4e0
ftakao2007 <ftakao2007@gmail.com>
2021-04-09 15:31:07 +0000

    update

--- build/symbolnode/digitalocean_centos8_symbolnode.sh
+++ build/symbolnode/digitalocean_centos8_symbolnode.sh
@@ -15,21 +15,20 @@ user=centos
 . ./setup_params_symbolnode_version
 
 # init_base
-ansible-playbook -i ~/hosts digitalocean_centos8_symbolnode.yml -u ${user} --private-key=~/.ssh/${key} \
-                 --extra-vars "ex_var_symbol_bootstrap_version=${symbol_bootstrap_version} \
-                               ex_var_symbol_cli_version=${symbol_cli_version} \
-                              "
+#ansible-playbook -i ~/hosts digitalocean_centos8_symbolnode.yml -u ${user} --private-key=~/.ssh/${key} \
+#                 --extra-vars "ex_var_symbol_bootstrap_version=${symbol_bootstrap_version} \
+#                               ex_var_symbol_cli_version=${symbol_cli_version} \
+#                              "
 
-exit
+#exit
 
 #pass=
 #bfaddress=
 #symbol_network=
 . ./setup_params_symbolnode_bootstrap
-exit
 
-#ansible-playbook -i ~/hosts digitalocean_centos8_symbolnode_bootstrap_make_workdir.yml -u ${user} --private-key=~/.ssh/${key} \
-#                 --extra-vars "ex_var_passwd=${pass} \
-#                               ex_var_bfaddress=${bfaddress} \
-#                               ex_var_symbol_network=${symbol_network} \
-#                              "
+ansible-playbook -i ~/hosts digitalocean_centos8_symbolnode_bootstrap_make_workdir.yml -u ${user} --private-key=~/.ssh/${key} -t ${symbol_network} \
+                 --extra-vars "ex_var_passwd=${pass} \
+                               ex_var_bfaddress=${bfaddress} \
+                               ex_var_symbol_network=${symbol_network} \
+                              "
