cea101be3fb6d3bd9b507097fefb57b3dfc81f76
ftakao2007 <ftakao2007@gmail.com>
2021-06-06 03:06:42 +0000

    add mincraft

---
+++ build/mincraft/digitalocean_centos8_mincraft.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+user=centos
+openjdk_version="1.8.0"
+
+#key=
+#address=
+. ./setup_params_mincraft
+
+# test
+#ssh -i ~/.ssh/${key} ${user}@${address}
+#exit
+
+
+# 基本的なソフトウェアのインストール
+./digitalocean_centos8_mincraft_base.sh $key $address $openjdk_version
+
+# symbol_bootstrapの設定と起動
+#./digitalocean_centos8_symbolnode_bootstrap.sh $key $address
+
+# symbol-cliの設定
+#./digitalocean_centos8_symbolnode_cli.sh
+
+# ハーベストの設定
+#./digitalocean_centos8_symbolnode_harvest.sh
---
+++ build/mincraft/digitalocean_centos8_mincraft_base.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+user=centos
+# $1 key=
+# $2 address=
+# . ./setup_params_symbolnode
+if [ -n "$1" ]; then key=$1; fi
+if [ -n "$2" ]; then address=$2; fi
+
+# test
+#ssh -i ~/.ssh/${key} ${user}@${address}
+#exit
+
+### 必要なソフトのインストール
+# $3 openjdk_version="1.8.0"
+#. ./setup_params_symbolnode_base
+if [ -n "$3" ]; then openjdk_version=$3; fi
+
+# init_base
+ansible-playbook -i ~/hosts digitalocean_centos8_mincraft_base.yml -u ${user} --private-key=~/.ssh/${key} \
+                 --extra-vars "ex_var_openjdk_version=${openjdk_version} \
+                              "
