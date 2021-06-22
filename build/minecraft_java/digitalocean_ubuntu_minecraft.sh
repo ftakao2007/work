e2a445d560dc799941897f706b803e38e1ce641e
ftakao2007 <ftakao2007@gmail.com>
2021-06-22 01:44:35 +0000

    update

--- build/minecraft_java/digitalocean_ubuntu_minecraft.sh
+++ build/minecraft_java/digitalocean_ubuntu_minecraft.sh
@@ -19,4 +19,4 @@ mx_memory_mb="1024"
 ./digitalocean_ubuntu_minecraft_base.sh $key $address $openjdk_version
 
 # minecraftのインストール
-#./digitalocean_ubuntu_minecraft_install.sh $key $address $forge_version $ms_memory_mb $mx_memory_mb
+./digitalocean_ubuntu_minecraft_install.sh $key $address $forge_version $ms_memory_mb $mx_memory_mb
