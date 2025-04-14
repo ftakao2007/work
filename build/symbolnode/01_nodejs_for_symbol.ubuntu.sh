7b3578deae41423271dcd5769b28341cddbcabca
ftakao2007 <ftakao2007@gmail.com>
2025-04-14 17:47:43 +0900

    update

--- build/symbolnode/01_nodejs_for_symbol.ubuntu.sh
+++ build/symbolnode/01_nodejs_for_symbol.ubuntu.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-IP_ADDR=",68.183.182.199"
+IP_ADDR=",159.89.196.242"
 user=ubuntu
 
 #PLAYBOOK_COMMAND="ansible-playbook"
 PLAYBOOK_COMMAND="/usr/local/pyenv/shims/python -m ansible playbook"
 
-#nvm_version=0.39.7
-nvm_version=0.40.1
-node_version=20.16.0
+nvm_version=0.40.2
+node_version=22.14.0
 ${PLAYBOOK_COMMAND} -i "${IP_ADDR}" 01_nodejs_for_symbol.ubuntu.yml -u ${user} --private-key=~/.ssh/id_rsa \
         --extra-vars "ex_var_nvm_version=${nvm_version} \
                       ex_var_node_version=${node_version}" \
