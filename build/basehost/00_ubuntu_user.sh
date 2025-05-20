03c9c2e77ea22190e7c5d8ad636802ca02cc3376
ftakao2007 <ftakao2007@gmail.com>
2025-05-20 22:05:19 +0900

    update

--- build/basehost/00_ubuntu_user.sh
+++ build/basehost/00_ubuntu_user.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-IP_ADDR=",68.183.182.199"
+IP_ADDR=",157.245.207.137"
+if [ -n "$1" ]; then IP_ADDR=",`head -1 $1`"; fi
+
 key="id_rsa"
 #PLAYBOOK_COMMAND="ansible-playbook"
 PLAYBOOK_COMMAND="/usr/local/pyenv/shims/python -m ansible playbook"
@@ -15,4 +17,9 @@ ${PLAYBOOK_COMMAND} -i ${IP_ADDR} 00_ubuntu_user.yml -u ${init_user} --private-k
                  --extra-vars "ex_var_user=${user} \
                                ex_var_user_id=${user_id} \
                                ex_var_group_id=${group_id}" \
-
+### use password
+# user
+#${PLAYBOOK_COMMAND} -i ${IP_ADDR} 00_ubuntu_user.yml -u ${init_user} --private-key=~/.ssh/${key} -k \
+#                 --extra-vars "ex_var_user=${user} \
+#                               ex_var_user_id=${user_id} \
+#                               ex_var_group_id=${group_id}" \
--- build/basehost/01_common_ubuntu.sh
+++ build/basehost/01_common_ubuntu.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-IP_ADDR=",68.183.182.199"
+IP_ADDR=",157.245.207.137"
+if [ -n "$1" ]; then IP_ADDR=",`head -1 $1`"; fi
+
 key="id_rsa"
 #PLAYBOOK_COMMAND="ansible-playbook"
 PLAYBOOK_COMMAND="/usr/local/pyenv/shims/python -m ansible playbook"
